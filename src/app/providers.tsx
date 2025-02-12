4210e7a61ad05974196da1373c4093748aa9a744
Minhye <miinnnn2@gmail.com>
2025-02-12 13:31:37 +0900

    chore: @tanstack/react-query 라이브러리 설치

---
+++ src/app/providers.tsx
@@ -0,0 +1,45 @@
+'use client';
+
+import {
+  isServer,
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+const makeQueryClient = () => {
+  return new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: 5 * 60 * 1000, // 5분
+        retry: false,
+        refetchOnWindowFocus: false,
+        refetchOnMount: true,
+      },
+    },
+  });
+};
+
+let browserQueryClient: QueryClient | undefined = undefined;
+
+const getQueryClient = () => {
+  if (isServer) {
+    return makeQueryClient();
+  } else {
+    if (!browserQueryClient) browserQueryClient = makeQueryClient();
+    return browserQueryClient;
+  }
+};
+
+const ReactQueryProvider = ({ children }: { children: React.ReactNode }) => {
+  const queryClient = getQueryClient();
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools />
+      {children}
+    </QueryClientProvider>
+  );
+};
+
+export default ReactQueryProvider;
